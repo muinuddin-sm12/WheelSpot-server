297086c78ec93d3eb17f624e8c80422884521a31
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-01-24 19:58:17 +0400

    refactor: car and order modules controler with catchAsync

--- src/app/modules/car/car.controller.ts
+++ src/app/modules/car/car.controller.ts
@@ -2,98 +2,54 @@
 import { Request, Response } from 'express';
 import { CarService } from './car.service';
 import { carValidationSchema } from './car.validation';
+import catchAsync from '../../../utils/catchAsync';
 
-const createCar = async (req: Request, res: Response) => {
-  try {
-    const carData = req.body;
-    const zodParseData = carValidationSchema.parse(carData);
-    const result = await CarService.createCarIntoDB(zodParseData);
-    res.status(200).json({
-      message: 'Car created successfully',
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
-const getAllCars = async (req: Request, res: Response) => {
-  try {
-    const result = await CarService.getAllCarsFromDB();
-    res.status(200).json({
-      message: 'Cars retrieved successfully',
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
-const getACar = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.carId;
-    const result = await CarService.getACarFromDB(id);
-    res.status(200).json({
-      message: 'Car retrieved successfully',
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
-const updateACar = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.carId;
-    const body = req.body;
-    const result = await CarService.updateACarIntoDB(id, body);
-    res.status(200).json({
-      message: 'Car updated successfully',
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
-const deleteACar = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.carId;
-    await CarService.deleteACarIntoDB(id);
-    res.status(200).json({
-      message: 'Car deleted successfully',
-      status: true,
-      data: {},
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
+const createCar = catchAsync(async (req: Request, res: Response) => {
+  const carData = req.body;
+  const zodParseData = carValidationSchema.parse(carData);
+  const result = await CarService.createCarIntoDB(zodParseData);
+  res.status(200).json({
+    message: 'Car created successfully',
+    status: true,
+    data: result,
+  });
+});
+const getAllCars = catchAsync(async (req: Request, res: Response) => {
+  const result = await CarService.getAllCarsFromDB();
+  res.status(200).json({
+    message: 'Cars retrieved successfully',
+    status: true,
+    data: result,
+  });
+});
+const getACar = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.carId;
+  const result = await CarService.getACarFromDB(id);
+  res.status(200).json({
+    message: 'Car retrieved successfully',
+    status: true,
+    data: result,
+  });
+});
+const updateACar = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.carId;
+  const body = req.body;
+  const result = await CarService.updateACarIntoDB(id, body);
+  res.status(200).json({
+    message: 'Car updated successfully',
+    status: true,
+    data: result,
+  });
+});
+const deleteACar = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.carId;
+  await CarService.deleteACarIntoDB(id);
+  res.status(200).json({
+    message: 'Car deleted successfully',
+    status: true,
+    data: {},
+  });
+});
 
 export const CarController = {
   createCar,
--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -3,52 +3,35 @@ import { OrderService } from './order.service';
 import { Request, Response } from 'express';
 import { orderValidationSchema } from './order.validation';
 import { OrderModel } from './order.model';
+import catchAsync from '../../../utils/catchAsync';
 
-const createOrder = async (req: Request, res: Response) => {
-  try {
-    const orderData = req.body;
-    const zodParsedData = orderValidationSchema.parse(orderData);
-    const result = await OrderService.createOrderIntoDB(zodParsedData);
-    res.status(200).json({
-      message: 'Order created successfully',
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      success: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
-const getRevenue = async (req: Request, res: Response) => {
-  try {
-    const revenue = await OrderModel.aggregate([
-      {
-        $group: {
-          _id: null,
-          totalRevenue: { $sum: '$totalPrice' },
-        },
+const createOrder = catchAsync(async (req: Request, res: Response) => {
+  const orderData = req.body;
+  const zodParsedData = orderValidationSchema.parse(orderData);
+  const result = await OrderService.createOrderIntoDB(zodParsedData);
+  res.status(200).json({
+    message: 'Order created successfully',
+    status: true,
+    data: result,
+  });
+});
+const getRevenue = catchAsync(async (req: Request, res: Response) => {
+  const revenue = await OrderModel.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalRevenue: { $sum: '$totalPrice' },
       },
-    ]);
-    res.status(200).json({
-      message: 'Revenue calculated successfully',
-      status: true,
-      data: {
-        totalRevenue: revenue[0].totalRevenue,
-      },
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: 'Something went wrong',
-      status: false,
-      error: error,
-      stack: error.stack,
-    });
-  }
-};
+    },
+  ]);
+  res.status(200).json({
+    message: 'Revenue calculated successfully',
+    status: true,
+    data: {
+      totalRevenue: revenue[0].totalRevenue,
+    },
+  });
+});
 
 export const OrderController = {
   createOrder,
