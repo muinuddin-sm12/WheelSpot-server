aba520ce4598c0f1893db3a7a87b28f7022ace33
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-05-03 05:29:45 +0400

    update: project title

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import router from './app/router';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ['https://l2-assignment-4-backend-kappa.vercel.app','http://localhost:5173', 'http://localhost:5175'], credentials: true }));
+app.use(cors({ origin: ['https://l2-assignment-4-backend-kappa.vercel.app','http://localhost:5173', 'https://l2-assignment-4-frontend.vercel.app'], credentials: true }));
 
 // application routes
 app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from L2-Assignment-4!');
+  res.send('Hello from Wheel Spot!');
 });
 export default app;
